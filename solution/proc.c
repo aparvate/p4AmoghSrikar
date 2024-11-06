96a53e35f01807e42a514b4b8af0a1f5d52ca96f
Srikar-Bhuvan <124732014+Srikar-Bhuvan@users.noreply.github.com>
2024-11-05 20:48:21 -0600

    Revert

--- solution/proc.c
+++ solution/proc.c
@@ -575,10 +575,9 @@ kill(int pid)
     if(p->pid == pid){
       p->killed = 1;
       // Wake process from sleep if necessary.
-      if(p->state == SLEEPING) {
+      if(p->state == SLEEPING) 
         p->state = RUNNABLE;
-        global_pass_update();
-      }
+      global_pass_update();
       release(&ptable.lock);
       return 0;
     }
